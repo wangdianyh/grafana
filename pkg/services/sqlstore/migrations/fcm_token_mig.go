67ceee65fe9f75f96b5059c97db1ba4dbc6de980
wangd <36665764+wangdianyh@users.noreply.github.com>
2020-03-27 16:43:41 -0300

    ...

--- pkg/services/sqlstore/migrations/fcm_token_mig.go
+++ pkg/services/sqlstore/migrations/fcm_token_mig.go
@@ -3,25 +3,7 @@ package migrations
 import . "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 
 func addFCMTokenMigration(mg *Migrator) {
-	/*
-		fcmToken := Table{
-			Name: "fcm_token",
-			Columns: []*Column{
-				{Name: "id", Type: DB_BigInt, IsPrimaryKey: true, IsAutoIncrement: true},
-				{Name: "token", Type: DB_NVarchar, Length: 300, Nullable: false},
-				{Name: "channel_id", Type: DB_NVarchar, Length: 190, Nullable: true},
-				{Name: "user_id", Type: DB_NVarchar, Length: 190, Nullable: false},
-				{Name: "created", Type: DB_DateTime, Nullable: false},
-				{Name: "updated", Type: DB_DateTime, Nullable: false},
-			},
-		}
 
-		// create table
-		mg.AddMigration("create fcm_token table", NewAddTableMigration(fcmToken))
-
-		// delete fcm_table
-		mg.AddMigration("drop fcm_token table", NewDropTableMigration(fcmToken.Name))
-	*/
 	// create a new table without channel_id column
 	fcmToken := Table{
 		Name: "fcm_token",
